7b15a351dceec15d27d73918e98f43204ffb57aa
AssortedFood <hamoon.b.94@gmail.com>
2024-08-04 14:33:21 +0100

    added positioning to resize_gui

--- resize_gui.py
+++ resize_gui.py
@@ -1,7 +1,7 @@
 import pygetwindow as gw
 import pyautogui
 
-def resize_and_position_mapleroyals(width, height):
+def resize_and_position_mapleroyals(width, height, position):
     # Find the MapleRoyals window
     windows = gw.getWindowsWithTitle('MapleRoyals')
     if not windows:
@@ -10,18 +10,21 @@ def resize_and_position_mapleroyals(width, height):
 
     mapleroyals_window = windows[0]
 
-    # Activate the window (bring it to the foreground)
     mapleroyals_window.activate()
-
-    # Resize the window
     mapleroyals_window.resizeTo(width, height)
-
-    # Get screen width and height
     screen_width, screen_height = pyautogui.size()
 
-    # Calculate the x position to center the window
-    x = (screen_width - width) // 2
-    y = 0  # Top of the screen
+    if position == "L":
+        x = 0
+        y = 0
+    elif position == "R":
+        x = screen_width - width
+        y = 0
+    elif position == "M":
+        x = (screen_width - width) // 2
+        y = 0
+    else:
+        print(f"Provided position ({position}) is invalid.")
 
     # Move the window to the top-center position
     mapleroyals_window.moveTo(x, y)
@@ -32,4 +35,4 @@ def resize_and_position_mapleroyals(width, height):
 size = 460
 w = size * 4
 h = size * 3
-resize_and_position_mapleroyals(w, h)
+resize_and_position_mapleroyals(w, h, "M")

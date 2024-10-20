732f1fba457c5845f7a31a8086ba5824b4b622f2
AssortedFood <hamoon.b.94@gmail.com>
2024-10-20 21:29:59 +0100

    Auto-commit: 2024-10-20 21:29:59

--- RunFarm.ps1
+++ RunFarm.ps1
@@ -2,8 +2,5 @@
 Set-Location "C:\Users\oxi\Projects\RoyalsMods"
 git pull
 
-# Wait for git pull to complete
-# Start-Sleep -Seconds 2
-
 # Reload the farm.ahk script with admin privileges
 Start-Process -FilePath "C:\Users\oxi\Projects\RoyalsMods\farm.ahk" -Verb RunAs

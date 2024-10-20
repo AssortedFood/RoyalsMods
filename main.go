7f03ada6f21c98f2cf47f3b4d6a3f0c0310c5e3b
AssortedFood <hamoon.b.94@gmail.com>
2024-10-20 22:29:05 +0100

    Auto-commit: 2024-10-20 22:29:05

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"github.com/kbinani/screenshot"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
+	// Define the region you want to capture
+	x1, y1 := 336, 0
+	x2, y2 := 1067, screenshot.GetDisplayBounds(0).Dy()
+
+	// Capture the screenshot of the specified region
+	bounds := image.Rect(x1, y1, x2, y2)
+	img, err := screenshot.CaptureRect(bounds)
+	if err != nil {
+		fmt.Println("Error capturing screenshot:", err)
+		return
+	}
+
+	// Save the screenshot to a file
+	file, err := os.Create("screenshot.png")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	err = png.Encode(file, img)
+	if err != nil {
+		fmt.Println("Error encoding image:", err)
+		return
+	}
+
+	fmt.Println("Screenshot saved to screenshot.png")
 }
